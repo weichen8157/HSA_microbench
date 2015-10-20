aeb38283f4ece52b1c997fbb5f510131691faf85
weichen8157 <weichen8157@gmail.com>
2015-10-20 14:33:40 +0800

    diff

--- mem_bandwidth/global/run.sh
+++ mem_bandwidth/global/run.sh
@@ -10,7 +10,7 @@ main()	{
 		/opt/amd/cloc/bin/hsailasm ./vector_copy.hsail -o ./vector_copy.brig
 		./global >> log
         echo ${file}>>record
-		#grep -r "Execution Period*" log >> record
+		grep -r "Execution Period*" log >> record
         grep -r "memory size" log >> record
         grep -r "op_count" log >> record
         grep -r "GB/s" log >> record
